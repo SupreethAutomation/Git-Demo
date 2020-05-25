116fdbbdf2071c1ef746b0baf44fcf210a009aa0
Dell <Dell@Supreeth-PC.Home>
2020-05-25 12:33:43 +0530

    commit GitDemo2

--- src/test/java/test/TestGitDemo.java
+++ src/test/java/test/TestGitDemo.java
@@ -3,6 +3,6 @@ package test;
 public class TestGitDemo {
 public static void main(String[] args) {
 	System.out.println("Git Demo");
-	
+	System.out.println("Git Demo2");
 }
 }
---
+++ src/test/java/test/TestGitDemo2.java
@@ -0,0 +1,8 @@
+package test;
+
+public class TestGitDemo2 {
+	public static void main(String[] args) {
+		System.out.println("Git Demo");
+		System.out.println("Git Demo2");
+	}
+}
