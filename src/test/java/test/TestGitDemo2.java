91c80aa6e617434b3c887423cb7bdbbaff2861b0
Dell <Dell@Supreeth-PC.Home>
2020-05-25 12:46:26 +0530

    addded sop3

--- src/test/java/test/TestGitDemo2.java
+++ src/test/java/test/TestGitDemo2.java
@@ -4,5 +4,6 @@ public class TestGitDemo2 {
 	public static void main(String[] args) {
 		System.out.println("Git Demo");
 		System.out.println("Git Demo2");
+		System.out.println("Git Demo3");
 	}
 }
